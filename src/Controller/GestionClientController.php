def698f8fd72e6db61eb044c66e02d055d62a45f
Thibault Laude <laudethibault_pro@outlook.fr>
2020-10-14 17:32:52 +0200

    Ajout de contenu dans GestionClientController

--- src/Controller/GestionClientController.php
+++ src/Controller/GestionClientController.php
@@ -2,7 +2,23 @@
 
 namespace APP\Controller;
 
-class GestionClientController
-{
-  //put your code here
+use APP\Model\GestionClientModel;
+use ReflectionClass;
+use \Exception;
+
+class GestionClientController {
+
+  public function chercheUn($params) {
+    // appel de la méthode find($id) de la classe Model adequate
+    $module = new GestionClientModel();
+    $id = filter_var(intval($params["id"]), FILTER_VALIDATE_INT);
+    $unClient = $modele->find($id);
+    if ($unClient) {
+      $r = new ReflectionClass($this);
+      include_once PATH_VIEW . str_replace('Controller', 'View', $r->getShortName()) . "/unClient.php";
+    } else {
+      throw new Exception("Client " . $id . " inconnu");
+    }
+  }
+
 }
