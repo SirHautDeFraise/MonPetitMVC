1e290cba9ecf7ad06b5a501b4bd5a1bbb08c37aa
Thibault Laude <laudethibault_pro@outlook.fr>
2020-10-14 17:08:29 +0200

    Correction plus ajout de la connexion BDD

--- Parametres.php
+++ Parametres.php
@@ -1,7 +1,7 @@
 <?php
 define('DATABASE_URL', "localhost");
-define('DATABASE_USER', "dussart_luke");
-define('DATABASE_PWD', "dussart_mvc64");
+define('DATABASE_USER', "laude");
+define('DATABASE_PWD', "P@ssw0rd");
 define('DATABASE_NAME', "clicom");
 define('CNSTRING', "mysql:host=" . DATABASE_URL . ";dbname=" . DATABASE_NAME . ";charset=UTF8");
 
